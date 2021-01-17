6aef7dd73fb0db737e568ceb124adcecad5291dc
Maxinger15 <47686016+Maxinger15@users.noreply.github.com>
2021-01-17 23:09:11 +0100

    Update __init__.lua

--- ext/Shared/__init__.lua
+++ ext/Shared/__init__.lua
@@ -6,7 +6,6 @@ local mounted = false
      --   print(SharedUtils:GetLevelName())
      --   name = SharedUtils:GetLevelName()
       --  if name ~= "Levels/MP_012/MP_012" and name ~= nil and mounted == false then
-            print("loading tanks")
             --Events:Dispatch(
             ---    "BundleMounter:LoadBundles",
             --    "Levels/MP_012/MP_012",
